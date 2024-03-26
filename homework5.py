cf899c46dd2a1e74c2add61911a72de61b67590d
MitrifanovPavel <163590983+MitrifanovPavel@users.noreply.github.com>
2024-03-26 20:28:58 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,18 @@
+my_list = ['banana', 'orange', 'pineapple', 'apple', 'blackberry']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'апельсин'
+print(my_list)
+
+my_dict = {
+    'banana': 'банан',
+    'orange': 'апельсин',
+    'pineapple': 'ананас',
+    'apple': 'яблоко',
+    'blackberry': 'ежевика'
+}
+print(my_dict)
+print(my_dict['banana'])
+my_dict['banana'] = 'бонан'
+print(my_dict)
